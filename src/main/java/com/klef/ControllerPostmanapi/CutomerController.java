f57758699c4696f132b82422df3a6212ef39ddd3
Dharmateja69 <ddharmateja69@gmail.com>
2024-10-29 07:49:21 +0530

    Describe your changes here

--- src/main/java/com/klef/ControllerPostmanapi/CutomerController.java
+++ src/main/java/com/klef/ControllerPostmanapi/CutomerController.java
@@ -5,6 +5,8 @@ package com.klef.ControllerPostmanapi;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 
 
@@ -30,6 +32,12 @@ public class CutomerController {
     public String home() {
         return "home";
     }
+    
+    @GetMapping("/sidu")
+    public String mainpage(@RequestParam String param) {
+        return new String();
+    }
+    
     
     
     
